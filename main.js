801ef046e042d7689fdbf70d1953fcdf56506185
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-07-10 18:25:11 -0500

    += y ++

---
+++ main.js
@@ -0,0 +1,18 @@
+/***
+
+VALORES FLASY
+0, null, undefinded, NaN, false, string vacio
+
+VALORES NULLISH
+null, undefined
+
+***/
+
+// Operadores += y ++
+
+let numero = 10;
+
+//numero = numero + 1;
+//numero += 1;
+numero++;
+console.log(numero);
