9764d273ad9b10d48c430bd991b86ea788586002
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-07-10 19:42:53 -0500

    Spread de Arrays

--- main.js
+++ main.js
@@ -35,8 +35,8 @@ null, undefined
 // Operador ||
 // FALSY : 0, null, undefinded, NaN, false, string vacio
 
-let usuario1 = "JC";
-let usuario2 = null;
+// let usuario1 = "JC";
+// let usuario2 = null;
 
 // if (usuario2) {
 //     console.log(usuario2);
@@ -44,5 +44,77 @@ let usuario2 = null;
 //     console.log("El usuario no existe");
 // }
 
-console.log( usuario1 || "El usuario no existe");
-console.log( usuario2 || "El usuario no existe");
+// console.log( usuario1 || "El usuario no existe");
+// console.log( usuario2 || "El usuario no existe");
+
+/**************************************** */
+/**************************************** */
+
+// Operador ??
+// NULLISH : null, undefinded
+
+// let numero = 0;
+// let numero2 = null;
+
+// console.log( numero ?? "El numero no es valido");
+// console.log( numero2 ?? "El numero no es valido");
+
+/**************************************** */
+/**************************************** */
+// Acceso condicional a un objeto
+
+//  const usuario = {
+//     nombre : "jc",
+//     cursos : {
+//         css : "aprobado",
+//         js : "aprobado"
+//     }
+//  }
+
+//  console.log( usuario?.cursos?.css || "La propiedad no existe");
+//  console.log( usuario?.trabajo?.youtube || "La propiedad no existe");
+
+
+/**************************************** */
+/**************************************** */
+// Desestructuracion de objetos
+
+//  const usuario = {
+//     nombre : "jc",
+//     edad : 30,
+//     cursos : {
+//         css : "aprobado",
+//         js : "aprobado"
+//     }
+//  }
+
+// let { nombre : nombreUsuario, edad, nacionalidad }= usuario;
+
+// console.log(nombreUsuario);
+
+/**************************************** */
+/**************************************** */
+// Desestructuracion de Arrays
+
+// const nombres = ["juan", "carlos", "maria", "jesus"];
+
+// const [ ,a, b] = nombres ;
+
+// console.log(a);
+
+
+
+/**************************************** */
+/**************************************** */
+// Spread de Arrays
+
+const nombres = ["juan", "carlos", "maria", "jesus"];
+
+const nombres2 = [...nombres];
+
+nombres2[0] = "jean";
+
+console.log(nombres);
+console.log(nombres2);
+
+
